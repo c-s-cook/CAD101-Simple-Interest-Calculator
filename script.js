8d416619752a872e83aa33b71e9cb881822a9657
Christopher Cook <christopherstephencook@gmail.com>
2021-12-09 03:59:35 +0000

    added comments into the code

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function compute()
     
     var year = new Date().getFullYear()+parseInt(years);  
    
-    //document.getElementById("result").innerHTML="<p>result" + interest +"</p>";
-
+    //the main block of output and calculations
     var resulting = "<p>Interest:</p><p>If you deposit <mark>$" + Intl.NumberFormat('en-US').format(principal) + "</mark><br> at an interest rate of <mark>" + rate + "%</mark><br> You will receive an amount of <mark><b>$" + Intl.NumberFormat('en-US').format(interest) + "</b></mark><br>in the year <mark>" + year + "</mark>.";
     
     document.getElementById("result").innerHTML=resulting;
-    //window.alert(resulting)
+
 
 }
 
+// fuction to validate the user-input of Prinicpal for a positive number
 function validateprin(prini){
     if (prini == "" || prini <= 0)
         alert("Enter a positive number");
@@ -26,6 +26,7 @@ function validateprin(prini){
 }
 
 
+// Updates the value present to the right of the slide-bar selector
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
